3dee5cc70cf20bf36921b7a7f2c462b7a7e18e56
yilmazali <yilmazalimetu@gmail.com>
2019-01-01 01:29:11 +0300

    Q1: 99.79% optimized

--- 1_TwoSum.py
+++ 1_TwoSum.py
@@ -10,19 +10,25 @@ class Solution:
         :rtype: List[int]
         """
 
-        num_set = set(nums)
+        num_set = {}
         for num_index, num in enumerate(nums):
-            print("target-num: " + str(target-num))
             if (target-num) in num_set:
-                first_index = num_index
-                if num == target-num:
-                    try:
-                        second_index = nums[first_index+1:].index(target-num)+first_index+1
-                    except Exception as e:
-                        continue
-                else:
-                    second_index = nums.index(target-num)
-                return [first_index, second_index]
+                return [num_set[target-num], num_index]
+            else:
+                num_set[num] = num_index
+
+
+            # print("target-num: " + str(target-num))
+            # if (target-num) in num_set:
+            #     first_index = num_index
+            #     if num == target-num and num in nums[first_index+1:]:
+            #         second_index = nums[first_index+1:].index(target-num)+first_index+1
+            #         # try:
+            #         # except Exception as e:
+            #         #     continue
+            #     else:
+            #         second_index = nums.index(target-num)
+            #     return [first_index, second_index]
 
 
 sol = Solution()
